08b1c275223d1289cebd36c32c1e73eb4c25e729
soheil <soheil_paper@yahoo.com>
2020-04-08 21:23:42 +0430

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<title>ESP8266 </title>
+  <h1>Communication With Server and ESP8266</h1>
+<p>This is a version of <a href="https://www.instructables.com/id/ESP8266-Communication-With-Server-and-ESP8266/" rel="nofollow">ESP8266 Communication With Server and ESP8266Hubot</a>, which is designed to be deployed on <a href="http://www.heroku.com" rel="nofollow">Heroku</a> with the The ESP8266 in Arduino IDE.</p>
+
+
+<head>
+<style type="text/css">
+div
+{
+   width:100px;
+   height:75px;
+   background-color:red;
+   border:1px solid black;
+}
+#div2
+{
+   transform:rotate(30deg);
+   -ms-transform:rotate(30deg); /* IE 9 */
+   -moz-transform:rotate(30deg); /* Firefox */
+   -webkit-transform:rotate(30deg); /* Safari and Chrome */
+   -o-transform:rotate(30deg); /* Opera */
+   background-color:yellow;
+}
+</style>
+</head>
+<body>
+<div> ESP8266!</div>
+<div id="div2">ESP8266 !</div>
+</body>
+</html>
