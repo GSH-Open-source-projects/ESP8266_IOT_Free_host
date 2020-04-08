eca1f7c4de4c02e08c8b33681e8d8b509957a6de
soheil <soheil_paper@yahoo.com>
2020-04-08 21:36:47 +0430

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
 <html>
 <title>ESP8266 </title>
-  <h1>Communication With Server and ESP8266</h1>
-<p>This is a version of <a href="https://www.instructables.com/id/ESP8266-Communication-With-Server-and-ESP8266/" rel="nofollow">ESP8266 Communication With Server and ESP8266Hubot</a>, which is designed to be deployed on <a href="http://www.heroku.com" rel="nofollow">Heroku</a> with the The ESP8266 in Arduino IDE.</p>
-
 
 
 <head>
@@ -27,11 +24,16 @@ div
 </style>
 </head>
 <body>
+    <div> ESP8266!</div>
+      <h1>Communication With Server and ESP8266</h1>
+<p>This is a version of <a href="https://www.instructables.com/id/ESP8266-Communication-With-Server-and-ESP8266/" rel="nofollow">ESP8266 Communication With Server and ESP8266Hubot</a>, which is designed to be deployed on <a href="http://www.heroku.com" rel="nofollow">Heroku</a> with the The ESP8266 in Arduino IDE.</p>
+
+
     <img width="800" height="480" src="https://i.stack.imgur.com/yHpwS.png"alt="https با ESP8266" srcset="" sizes="(max-width: 800px) 100vw, 800px">
-    
+    <div id="div2">ESP8266 !</div>
     <img width="800" height="480" src="https://i.stack.imgur.com/e3QVs.png"alt="https با ESP8266" srcset="" sizes="(max-width: 800px) 100vw, 800px"> 
     
-<div> ESP8266!</div>
-<div id="div2">ESP8266 !</div>
+
+
 </body>
 </html>
