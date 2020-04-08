8b2b59c0de35fd4f98065bffc5c8be8928df7e84
soheil <soheil_paper@yahoo.com>
2020-04-08 21:33:31 +0430

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 <p>This is a version of <a href="https://www.instructables.com/id/ESP8266-Communication-With-Server-and-ESP8266/" rel="nofollow">ESP8266 Communication With Server and ESP8266Hubot</a>, which is designed to be deployed on <a href="http://www.heroku.com" rel="nofollow">Heroku</a> with the The ESP8266 in Arduino IDE.</p>
 
 
+
 <head>
 <style type="text/css">
 div
@@ -26,6 +27,10 @@ div
 </style>
 </head>
 <body>
+    <img width="800" height="480" src="https://i.stack.imgur.com/yHpwS.png"alt="https با ESP8266" srcset="" sizes="(max-width: 800px) 100vw, 800px">
+    
+    <img width="800" height="480" src="https://i.stack.imgur.com/e3QVs.png"alt="https با ESP8266" srcset="" sizes="(max-width: 800px) 100vw, 800px"> 
+    
 <div> ESP8266!</div>
 <div id="div2">ESP8266 !</div>
 </body>
